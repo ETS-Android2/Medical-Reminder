dff53c7df8e9c3b538140a68969e0b1cde2e8c15
Karam Ibrahim <karamzeroo@gmail.com>
2022-03-06 15:00:37 +0200

    Commit
    Signed-off-by: Karam Ibrahim <karamzeroo@gmail.com>

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import in.akshit.horizontalcalendar.HorizontalCalendarView;
+import in.akshit.horizontalcalendar.Tools;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HorizontalCalendarView calendarView = findViewById(R.id.calendar);
+
+        Calendar starttime = Calendar.getInstance();
+        starttime.add(Calendar.MONTH,-6);
+
+        Calendar endtime = Calendar.getInstance();
+        endtime.add(Calendar.MONTH,6);
+
+        ArrayList datesToBeColored = new ArrayList();
+        datesToBeColored.add(Tools.getFormattedDateToday());
+
+        calendarView.setUpCalendar(starttime.getTimeInMillis(),
+                endtime.getTimeInMillis(),
+                datesToBeColored,
+                new HorizontalCalendarView.OnCalendarListener() {
+                    @Override
+                    public void onDateSelected(String date) {
+                        Toast.makeText(MainActivity.this,date+" clicked!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
