0ec5d6f095460af158b7b5668ea2f52d87b3ac56
Shrouk Mamdouh <ShroukMamdouh_127@yahoo.com>
2022-03-09 01:34:18 +0200

    EditMedicationDrugActivity.java and activity_edit_medication_drug.xml

--- app/src/main/java/com/example/androidproject/EditMedicationDrugActivity.java
+++ app/src/main/java/com/example/androidproject/EditMedicationDrugActivity.java
@@ -3,18 +3,12 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class DrugScreenActivity extends AppCompatActivity {
-
-
+public class EditMedicationDrugActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drug_screen);
-//============//
-
-
+        setContentView(R.layout.activity_edit_medication_drug);
     }
 }
