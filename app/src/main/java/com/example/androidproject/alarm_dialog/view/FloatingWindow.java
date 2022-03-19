6578683d42b71e9b4b13ae8cee63fd5d865889f8
Karam Ibrahim <karamzeroo@gmail.com>
2022-03-20 00:37:08 +0200

    added Alarm dialog with Sound working by work manager  and getting the time for all Medicines for today<o.o>
    FloatingWindowInterface
    FloatingWindow
    FloatingWindowPresenter
    FloatingWindowPresenterInterface
    
    Signed-off-by: Karam Ibrahim <karamzeroo@gmail.com>

--- app/src/main/java/com/example/androidproject/alarm_dialog/view/FloatingWindow.java
+++ app/src/main/java/com/example/androidproject/alarm_dialog/view/FloatingWindow.java
@@ -7,8 +7,10 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.example.androidproject.R;
 import com.example.androidproject.alarm_dialog.presenter.FloatingWindowPresenter;
@@ -16,7 +18,6 @@ import com.example.androidproject.alarm_dialog.presenter.FloatingWindowPresenter
 import com.example.androidproject.home.presenter.HomePresenter;
 import com.example.androidproject.local_data.LocalList;
 import com.example.androidproject.repo.ListRepository;
-
 import java.util.ArrayList;
 
 public class FloatingWindow extends AppCompatActivity implements FloatingWindowInterface {
@@ -32,7 +33,13 @@ public class FloatingWindow extends AppCompatActivity implements FloatingWindowI
         setContentView(R.layout.activity_floating);
         presenterInterface = new FloatingWindowPresenter(this, ListRepository.getInstance(this , LocalList.getInstance(this)));
 
+        setMediaPlayer();
 
+        presenterInterface.getTimes();
+
+    }
+
+    void setMediaPlayer(){
         mMediaPlayer = new MediaPlayer();
         mMediaPlayer = MediaPlayer.create(this, R.raw.cleopatra);
         mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
