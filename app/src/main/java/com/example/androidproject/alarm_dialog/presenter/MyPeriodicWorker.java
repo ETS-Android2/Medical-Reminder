b7653033836cfb1475b76ffa5966e6ccc5f0fe4f
Rawan_Ashraf <rbahnasy98@gmail.com>
2022-03-22 23:47:18 +0200

    Refill Reminder version2

---
+++ app/src/main/java/com/example/androidproject/alarm_dialog/presenter/MyPeriodicWorker.java
@@ -0,0 +1,31 @@
+package com.example.androidproject.alarm_dialog.presenter;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.androidproject.local_data.LocalDataBase;
+import com.example.androidproject.repo.ListRepository;
+
+
+public class MyPeriodicWorker extends Worker {
+    Context context;
+    public MyPeriodicWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i("TAG", "MyPeriodicWorker doWork: ");
+        ListRepository repository = ListRepository.getInstance(context, LocalDataBase.getInstance(context));
+        repository.updateManagerTimes();
+
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/androidproject/alarm_dialog/presenter/MyWorker.java
@@ -0,0 +1,60 @@
+package com.example.androidproject.alarm_dialog.presenter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.androidproject.alarm_dialog.view.FloatingWindow;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class MyWorker extends Worker {
+
+
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        //implement the logic here
+
+        Context context =  getApplicationContext();
+        Log.i("TAG", "doWork: ");
+
+        Handler handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                Toast.makeText(context, "Hello from doWork: ", Toast.LENGTH_SHORT).show();
+
+                Log.i("TAG", "Hello from doWork: from handler ");
+
+                Intent intent = new Intent(context, FloatingWindow.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        };
+
+        handler.sendEmptyMessage(0);
+
+
+
+        // Indicate whether the work finished successfully with the Result
+        return Result.success();
+    }
+
+}
