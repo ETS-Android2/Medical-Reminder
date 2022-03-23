5453f35b012573f3bc9d1a91c89ecd9a41776683
Karam Ibrahim <karamzeroo@gmail.com>
2022-03-23 02:19:36 +0200

    DrugScreenActivity that shows the selected medicine details :)
    Signed-off-by: Karam Ibrahim <karamzeroo@gmail.com>

--- app/src/main/java/com/example/androidproject/registration/RegisterRepository/RegisterRepositoryInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.androidproject.registration.RegisterRepository;
-
-public interface RegisterRepositoryInterface {
-
-    public void register(String emailAddress, String Password);
-}
